93987057bc916019773350487930aa4f7516b040
Aditya <adityakryadav10@gmail.com>
2026-09-02 18:15:17 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center text-3xl font-bold">
+      Ganesh build a React app with Vite for gradcommerce
+    </div>
+  );
+}
+
+export default App;
